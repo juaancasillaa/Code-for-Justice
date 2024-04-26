f34b02e83a43ddd84305be1c32d4687ebbcf6535
juaancasillaa <juanacasilla16@outlook.com>
2024-04-25 21:28:52 -0400

    adding sidebar

--- contact-juan/contact.js
+++ contact-juan/contact.js
@@ -53,3 +53,16 @@ form.addEventListener('submit', function(e) {
     form.submit();
   }
 });
+
+//creating a function that when the hamburger menu gets clicked the style of the side bar style will change to 'flex'
+
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex';
+}
+//creation a function that when close icon gets clicked the side bar will no longer appear
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none';
+}
